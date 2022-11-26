27db9a7be7d1a35373b1f70848c78f76a15d5b4c
Eduardo (Kenny8855) Assunção <39742355+eduardoAssuncao@users.noreply.github.com>
2022-11-26 19:03:32 -0300

    Add files via upload

--- Atividade_04/JS/q5.js
+++ Atividade_04/JS/q5.js
@@ -1,10 +1,10 @@
 const form = document.querySelector("form");
-// Use insert() function to insert the number in textview.  
+ 
 function inserir(num){  
     document.form.textview.value = document.form.textview.value + num;  
 }  
   
-// Use equal() function to return the result based on passed values.  
+
 function igual()  {  
     var exp = document.form.textview.value;  
         if(exp)  
@@ -13,10 +13,10 @@ function igual()  {
         }  
 }  
   
-/* Here, we create a backspace() function to remove the number at the end of the numeric series in textview. */  
+
 function backspace()  {  
     var exp = document.form.textview.value;  
-    document.form.textview.value = exp.substring(0, exp.length - 1); /* remove the element from total length ? 1 */  
+    document.form.textview.value = exp.substring(0, exp.length - 1);  
 }  
 
 function apagar(){
